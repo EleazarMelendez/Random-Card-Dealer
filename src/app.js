615a8c4343ea7b1beee0c7da9e1ec200285aa890
EleazarMelendez <edavidmelendez@gmail.com>
2023-02-25 01:55:47 +0000

    Created random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function changeTheCard() {
+  const AcetoKing = document.querySelector("#face");
+  const TopOfCard = document.querySelector("#top-suite");
+  const BottomOfCard = document.querySelector("#bottom-suite");
+
+  const possibleSuites = ["♦", "♥", "♠", "♣"];
+  const possibleFaces = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const RandomFaceValue = Math.floor(Math.random() * possibleFaces.length);
+  const RandomSuiteValue = Math.floor(Math.random() * possibleSuites.length);
+
+  AcetoKing.innerText = possibleFaces[RandomFaceValue];
+  TopOfCard.innerText = possibleSuites[RandomSuiteValue];
+  BottomOfCard.innerText = TopOfCard.innerText;
+
+  if (RandomSuiteValue <= 1) {
+    AcetoKing.style.color = "red";
+    TopOfCard.style.color = "red";
+    BottomOfCard.style.color = "red";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  changeTheCard();
+
+  let clickyButton = document.getElementById("hitMe");
+  clickyButton.addEventListener("click", changeTheCard);
 };
+
+setTimeout(function() {
+  location = "";
+}, 5000);
